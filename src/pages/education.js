11aaf2b91b436fe796ff877029f5daede6eef14a
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-08-26 00:16:12 +0300

    Adds a graphql query for the education, creates the content

---
+++ src/pages/education.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default ({ data }) => {
+  return (
+    <div className="education-page">
+      <h1>Education</h1>
+      <h2>Schools</h2>
+      <div className="schools">
+        {data.nodeEducation.relationships.field_school.map((course, index) => {
+          return (
+            <div className="school" key={index}>
+              <div className="school-education-title">{course.field_title}</div>
+              <div className="school-duration">{course.field_duration}</div>
+              <div className="school-name">{course.field_school_name}</div>
+            </div>
+          );
+        })}
+      </div>
+      <h2>Courses</h2>
+      <div className="courses">
+        {data.nodeEducation.relationships.field_courses.map((course, index) => {
+          return (
+            <div className="course" key={index}>
+              <div className="course-title">{course.field_title}</div>
+              <div className="course-date">{course.field_month_year}</div>
+              <div className="course-educator">{course.field_educator}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query Education {
+    nodeEducation {
+      id
+      title
+      relationships {
+        field_courses {
+          field_educator
+          field_month_year
+          field_title
+        }
+        field_school {
+          field_duration
+          field_school_name
+          field_title
+        }
+      }
+    }
+  }
+`;
