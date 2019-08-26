b30f97a27ad781767e4dc4c16a0570bbcfeec76c
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-08-26 23:26:28 +0300

    Adds preliminary work experience page query and contents

---
+++ src/pages/experience.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default ({ data }) => {
+  return (
+    <div className="experience-page">
+      <h1>Work experience</h1>
+      <div className="work-experiences">
+        {data.allNodeWorkExperience.nodes.map((position, index) => {
+          return (
+            <div className="experience" key={`experience-${index}`}>
+              <div className="position-title">{position.title}</div>
+              <div className="position-company">
+                {position.field_company.title}
+              </div>
+              <div className="position-duration">
+                {position.field_start_date}
+                {position.field_end_date ? " - " + position.field_end_date : ""}
+              </div>
+              <div className="position-description">
+                {position.body ? position.body.summary : ""}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query Experience {
+    allNodeWorkExperience(sort: { fields: field_start_date, order: DESC }) {
+      nodes {
+        title
+        body {
+          summary
+        }
+        field_company {
+          title
+          uri
+        }
+        field_end_date(formatString: "MM/YYYY")
+        field_start_date(formatString: "MM/YYYY")
+      }
+    }
+  }
+`;
