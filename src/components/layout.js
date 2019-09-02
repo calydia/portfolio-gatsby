c706f7898a394ac257c4dd45acd016d645467590
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-09-03 00:25:31 +0300

    Adds footer to the layout file, adds background image functionality

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,88 @@
 import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import BackgroundImage from 'gatsby-background-image';
 import ThemeContext from '../context/ThemeContext';
 import Header from "../components/header";
+import Footer from "../components/footer"
 
 export default ({ children }) => {
+
+
+
+  const BackgroundSection = ({ className }) => (
+
+    <StaticQuery
+      query={graphql`
+      query Backgrounds {
+        bg0: file(relativePath: { eq: "bg01.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        bg1: file(relativePath: { eq: "bg02.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        bg2: file(relativePath: { eq: "bg03.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      
+      `}
+      render={data => {
+
+        const getRandomInt = (max) => {
+          return Math.floor(Math.random() * Math.floor(max));
+        }
+        const random = 'bg' + getRandomInt(3);
+
+        // Set ImageData.
+        let imageData = data.bg0.childImageSharp.fluid
+        if (random === "bg1") {
+          imageData = data.bg1.childImageSharp.fluid
+        }
+        if (random === "bg2") {
+          imageData = data.bg2.childImageSharp.fluid
+        }
+
+
+        return (
+          <BackgroundImage
+            Tag="section"
+            className={className}
+            fluid={imageData}
+            backgroundColor={`#040e18`}
+          >
+            <div className="content-container">
+              <Header />
+              {children}
+              <Footer />
+            </div>
+          </BackgroundImage>
+        )
+      }}
+    />
+  )
+
 
   return (
     <ThemeContext.Consumer>
       {theme => (
         <div className={theme.dark ? 'dark' : 'light'}>
-          <Header />
-          {children}
+          <BackgroundSection>
+            
+            {children}
+            
+          </BackgroundSection>
         </div>
       )}
     </ThemeContext.Consumer>
