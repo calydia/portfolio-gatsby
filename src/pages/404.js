428797d0f1c6e310fa3d7a258f395444be424f58
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-09-03 16:43:48 +0300

    Creates a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "../components/layout";
+
+export default () => {
+  return (
+    <Layout>
+      <div className="layout-page front-page">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Page not found | Portfolio - Sanna Mäkinen</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+
+        <h1>Page not found</h1>
+        <div className="page-content">
+          <p>
+            Page not found. Please use the navigation to find the page you were
+            looking for.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
