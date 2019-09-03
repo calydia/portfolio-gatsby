9b9bde4356765b9603b61a39b466c7ec1cca0618
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-09-03 16:44:23 +0300

    Adds content to the front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet";
 import Layout from "../components/layout";
 
 export default () => {
-
   return (
     <Layout>
       <div className="layout-page front-page">
@@ -15,7 +14,23 @@ export default () => {
 
         <h1>Hello!</h1>
         <div className="page-content">
-          <p>I'm Sanna. I currently work as a web developer at <a href="https://www.karhuhelsinki.fi" target="_blank" rel="noopener noreferrer">Karhu Helsinki</a>.</p>
+          <p>
+            I'm Sanna. I currently work as a web developer at{" "}
+            <a
+              href="https://www.karhuhelsinki.fi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Karhu Helsinki
+            </a>
+            , mainly working with Drupal. I like to work on both the backend and
+            the frontend but if I had to choose, I would definitely pick the
+            frontend. There's just something about bringing the design to life.
+          </p>
+          <p>
+            Please take a look around and feel free to contact me if you have
+            any questions or if you just want to say hello!
+          </p>
         </div>
       </div>
     </Layout>
