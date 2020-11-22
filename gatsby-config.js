1f3c662551985743d3ed6139d77021808fb049e6
Sanna Mäkinen <sanna.makinen@gmail.com>
2020-11-22 13:06:33 +0200

    Adds purpose any maskable

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,6 +114,10 @@ module.exports = {
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
+        icon_options: {
+          // For all the options available, please see the additional resources below.
+          purpose: `any maskable`,
+        },
         crossOrigin: `use-credentials`,
       },
     },
